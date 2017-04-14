3c7b595c8a2247206a890eeca5536af9c43e0a9b
Class Account <skz.zxc@gmail.com>
2017-04-13 17:02:41 -0700

    setup boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 
+#include <GL/glew.h>
+
 #include <GLFW/glfw3.h>
 
 int main (int argc, const char* argv[]) {
+	glfwInit();
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
+	glfwTerminate();
 	return 0;
 }
