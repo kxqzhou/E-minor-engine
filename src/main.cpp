3d38086f015810191cfd8f97a25735adf80fb02b
Class Account <skz.zxc@gmail.com>
2017-04-14 14:04:40 -0700

    using CGL viewer

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,15 @@
 
-#include <GL/glew.h>
+#include "CGL/CGL.h"
+#include "CGL/viewer.h"
 
-#include <GLFW/glfw3.h>
-
-#include <iostream>
+using namespace CGL;
 
 int main (int argc, char* argv[]) {
-	glfwInit();
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
-	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	Viewer viewer = Viewer();
 
-	GLFWwindow* window = glfwCreateWindow( 800, 600, "E-Minor Engine", NULL, NULL );
-	if (!window) {
-		std::cout << "Failed to create GLFW window\n";
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent( window );
+	viewer.init();
 
-	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK) {
-		std::cout << "Failed to initialize GLEW\n";
-		return -1;
-	}
+	viewer.start();
 
-	int width, height;
-	glfwGetFramebufferSize( window, &width, &height );
-
-	glViewport( 0, 0, width, height );
-
-	while ( !glfwWindowShouldClose( window ) ) {
-		glfwPollEvents();
-		glfwSwapBuffers( window );
-	}
-
-	glfwTerminate();
 	return 0;
 }
