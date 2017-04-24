0eb6d56cf1ca8982b7e2d079a5742a1ed4638f50
Kyle <skz.zxc@gmail.com>
2017-04-23 21:57:12 -0700

    clearing buffer

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main (int argc, char* argv[]) {
 
 	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
 	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
 	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
 	GLFWwindow* window = glfwCreateWindow( 800, 600, "E-Minor Engine", NULL, NULL );
@@ -34,6 +34,10 @@ int main (int argc, char* argv[]) {
 
 	while ( !glfwWindowShouldClose( window ) ) {
 		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers( window );
 	}
 
