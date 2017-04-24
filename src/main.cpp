e406f122c2e6ed6b1c0934d052e93ffce1e6b4d8
Kyle <skz.zxc@gmail.com>
2017-04-23 21:22:18 -0700

    finally got it to compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,41 @@
 
-#include "CGL/CGL.h"
-#include "CGL/viewer.h"
+#include <GL/glew.h>
 
-using namespace CGL;
+#include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main (int argc, char* argv[]) {
-	Viewer viewer = Viewer();
+	glfwInit();
 
-	viewer.init();
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
+	GLFWwindow* window = glfwCreateWindow( 800, 600, "E-Minor Engine", NULL, NULL );
+	if (!window) {
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent( window );
 
-	viewer.start();
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Failed to initialize GLEW\n";
+		return -1;
+	}
 
+	int width, height;
+	glfwGetFramebufferSize( window, &width, &height );
+
+	glViewport( 0, 0, width, height );
+
+	while ( !glfwWindowShouldClose( window ) ) {
+		glfwPollEvents();
+		glfwSwapBuffers( window );
+	}
+
+	glfwTerminate();
 	return 0;
 }
