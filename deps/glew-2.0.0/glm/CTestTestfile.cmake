38828c6ff7885477ebd32390f1d3e3d0dd1e583d
Ricky Herrera <rherrera@berkeley.edu>
2017-05-05 22:46:29 -0700

    shaders and lighting

---
+++ deps/glew-2.0.0/glm/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ricky/desktop/E-minor-engine/deps/glm
+# Build directory: /Users/ricky/desktop/E-minor-engine/deps/glew-2.0.0/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")
---
+++ deps/glew-2.0.0/glm/test/gtx/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ricky/desktop/E-minor-engine/deps/glm/test/gtx
+# Build directory: /Users/ricky/desktop/E-minor-engine/deps/glew-2.0.0/glm/test/gtx
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
